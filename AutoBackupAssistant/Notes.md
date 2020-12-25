cdef0f9e846c35631df5dc582ca3f4378f8a1d2b
derblaettgen <sascha@blaettgen.com>
2020-12-25 18:42:47 +0100

    Add project files.

---
+++ AutoBackupAssistant/Notes.md
@@ -0,0 +1,6 @@
+## TODO
+- [ ] exclude specific (config?) files and folders
+- [ ] implement ignore patterns (gitignore)
+- [ ] support gitignore and custom ignore files
+- [ ] support for googledrive
+- [ ] 
---
+++ AutoBackupAssistant/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ionic.Zip;
+
+namespace AutoBackupAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFolder = @"C:\Temp\AutoBackupTest";
+            string targetZipFile = @"C:\Temp\AutoBackupTest.zip";
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddDirectory(sourceFolder);
+                zip.Save(targetZipFile);
+            }
+        }
+    }
+}
