2159846e23cc380d368e3c33e973d246d9b5ce50
derblaettgen <sascha@blaettgen.com>
2020-12-27 10:25:34 +0100

    Added Configuration

---
+++ AutoBackupAssistant/ConfigKeys.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoBackupAssistant
+{
+    public sealed class ConfigKeys
+    {
+        public string sourceFolder { get; private set; }
+        public string targetZipFile { get; private set; }
+
+        public ConfigKeys()
+        {
+            sourceFolder = "sourceFolder";
+            targetZipFile = "targetZipFile";
+        }
+    }
+}
--- AutoBackupAssistant/Program.cs
+++ AutoBackupAssistant/Program.cs
@@ -1,19 +1,54 @@
 ﻿using Ionic.Zip;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AutoBackupAssistant
 {
     class Program
     {
+        public static Dictionary<string, string> LoadConfig(string settingfile)
+        {
+            var dic = new Dictionary<string, string>();
+
+            if (File.Exists(settingfile))
+            {
+                var settingdata = File.ReadAllLines(settingfile);
+                for (var i = 0; i < settingdata.Length; i++)
+                {
+                    var setting = settingdata[i];
+                    var sidx = setting.IndexOf("=");
+                    if (sidx >= 0)
+                    {
+                        var skey = setting.Substring(0, sidx);
+                        var svalue = setting.Substring(sidx + 1);
+                        if (!dic.ContainsKey(skey))
+                        {
+                            dic.Add(skey, svalue);
+                        }
+                    }
+                }
+            }
+
+            return dic;
+        }
+
         static void Main(string[] args)
         {
-            string sourceFolder = @"C:\Temp\AutoBackupTest";
-            string targetZipFile = @"C:\Temp\AutoBackupTest.zip";
+            ConfigKeys configKeys = new ConfigKeys();
+            var config = LoadConfig(@"./aba.cfg");
 
-            using (ZipFile zip = new ZipFile())
+            string sourceFolder;
+            string targetZipFile;
+
+            if (!config.TryGetValue(configKeys.sourceFolder, out sourceFolder) || !config.TryGetValue(configKeys.targetZipFile, out targetZipFile))
             {
-                zip.AddDirectory(sourceFolder);
-                zip.Save(targetZipFile);
-            }
+                System.Console.WriteLine("ERR: Config not found");
+                return;
+            };
+
+            ZipFile zip = new ZipFile();
+            zip.AddDirectory(sourceFolder);
+            zip.Save(targetZipFile);
         }
     }
 }
